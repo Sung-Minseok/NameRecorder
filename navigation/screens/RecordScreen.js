b55862d109aef43cd96bdaca53e323accb593d7f
minseok <alstjrsung@gmail.com>
2022-01-04 15:21:44 +0900

    create bottom navigator

---
+++ navigation/screens/RecordScreen.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+export default function RecordScreen({navigation}) {
+    return(
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text 
+                onPress={() => alert('aaaa')} 
+                style={{ fontSize: 26, fontWeight: 'bold'}}>
+                Record Screen
+            </Text>
+        </View>
+    )
+}
